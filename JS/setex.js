cab315e66dac4b93f91092e6d0273dd7de10d9a7
Shashwat-Chauhan <shashwatchauhan12@gmail.com>
2024-10-16 10:05:22 +0530

    set

---
+++ JS/setex.js
@@ -0,0 +1,16 @@
+const myset = new Set()
+console.log(myset);
+myset.add(24);
+myset.add(21);
+myset.add(17);
+myset.add(24);
+myset.add(21);
+console.log(myset)
+
+myset.add(69)
+console.log(myset)
+const num = [1,2,3,4,5,6]
+const a1 = num.filter((n)=>{
+    return (n%2 == 0);
+})
+console.log(a1);
